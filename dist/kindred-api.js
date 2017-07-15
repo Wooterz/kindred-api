ce085a8095a0ed2bdc6f2ab0e6cd18311a688f54
Chau Nguyen <chautnguyen96@gmail.com>
2017-07-14 22:03:03 -0700

    Update printResponseDebug headers

--- dist/kindred-api.js
+++ dist/kindred-api.js
@@ -472,7 +472,9 @@
     if (showHeaders) {
       console.log({
         'x-rate-limit-type': response.headers['x-rate-limit-type'],
+        'x-app-rate-limit': response.headers['x-app-rate-limit'],
         'x-app-rate-limit-count': response.headers['x-app-rate-limit-count'],
+        'x-method-rate-limit': response.headers['x-method-rate-limit'],
         'x-method-rate-limit-count': response.headers['x-method-rate-limit-count'],
         'x-rate-limit-count': response.headers['x-rate-limit-count'],
         'retry-after': response.headers['retry-after']
